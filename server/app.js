86ae2531d726746dfecd9c003a0aed1d6b56207c
abhit-rana <abhit20421@iiitd.ac.in>
2023-11-05 23:37:04 +0530

    added login and register apis

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
 import userRouter from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
+import errorMiddleware from "./middlewares/error.js";
 
 const app = express();
 
@@ -10,7 +12,10 @@ config({
 
 //middlewares
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/v1/users", userRouter);
+// app.use("/api/v1/student", filterRouter)
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Nice Working");
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,20 @@
 import express from "express";
+import isAuthenticated from "../middlewares/auth.js";
 
-import register from "../controllers/userController.js";
+import {
+  register,
+  login,
+  getMyProfile,
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
 
+userRouter.get("/login", login);
+
 // userRouter.post("/login", login);
 
-// userRouter.get("/profile", getMyProfile);
+userRouter.get("/myProfile", isAuthenticated, getMyProfile);
 
 export default userRouter;
