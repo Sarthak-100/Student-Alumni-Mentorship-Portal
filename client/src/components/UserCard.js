8fa4267d41ff847bed7c3ccd512e3978c807c80d
Sarthak-100 <sarthak20576@iiitd.ac.in>
2023-11-17 21:31:11 +0530

    feat: styling added to userCard and profile dialog box

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardContent,
@@ -7,62 +7,114 @@ import {
   Typography,
   CardActions,
   IconButton,
+  Grid,
 } from "@mui/material";
 import ChatIcon from "@mui/icons-material/Chat";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import axios from "axios";
-import { useUserContext } from "../context/UserContext";
-import { useReceiverIdContext } from "../context/ReceiverIdContext";
-import { useNavigate } from "react-router-dom";
+import ProfileDisplay from "./ProfileDisplay"; // Importing your ProfileDisplay component
 
 const UserCard = (props) => {
-  const navigate = useNavigate();
-  const { user } = useUserContext();
-  const { setReceiverIdValue } = useReceiverIdContext();
+  const [openProfile, setOpenProfile] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
 
-  const handleChat = () => {
-    setReceiverIdValue(props.cardUser?._id);
-    navigate("/chat/welcome");
+  const handleProfile = () => {
+    setSelectedUser(props.cardUser); // Store the selected user data
+    setOpenProfile(true); // Open the profile dialog
   };
 
-  // Destructure the nested location object
   const { city, state, country } = props.cardUser?.location || {};
 
-  // Log props.cardUser to check its structure and the organization data
-  console.log("Card User Data:", props.cardUser);
+  const cardStyle = {
+    maxWidth: 300,
+    margin: "20px auto",
+    border: "1px solid #ddd",
+    borderRadius: "8px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+  };
+
+  const titleStyle = {
+    fontFamily: "Arial, sans-serif",
+    fontWeight: "bold",
+    color: "#333",
+  };
+
+  const subheaderStyle = {
+    fontFamily: "Arial, sans-serif",
+    color: "#555",
+  };
+
+  const contentStyle = {
+    fontFamily: "Arial, sans-serif",
+    color: "#777",
+  };
 
   return (
-    <Card>
+    <Card style={cardStyle}>
       <CardHeader
         avatar={<Avatar>{props.cardUser?.name.charAt(0)}</Avatar>}
-        title={props.cardUser?.name}
-        subheader={props.cardUser?.email}
+        title={
+          <Typography variant="h6" style={titleStyle}>
+            {props.cardUser?.name}
+          </Typography>
+        }
+        subheader={
+          <Typography variant="body2" style={subheaderStyle}>
+            {props.cardUser?.email}
+          </Typography>
+        }
       />
       <CardContent>
-        <Typography variant="body1">
-          Current Work: {props.cardUser?.work.role}
-        </Typography>
-        <Typography variant="body1">Branch: {props.cardUser?.branch}</Typography>
-        <Typography variant="body1">Batch: {props.cardUser?.batch}</Typography>
-
-        {/* Render Organization Name */}
-        <Typography variant="body1">
-          Organization: {props.cardUser?.work.organization}
-        </Typography>
-
-        {/* Render Location details */}
-        <Typography variant="body1">
-          Location: {city}, {state}, {country}
-        </Typography>
+        {/* Render details */}
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Typography variant="body1" style={contentStyle}>
+              <strong>Current Work:</strong> {props.cardUser?.work.role}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="body1" style={contentStyle}>
+              <strong>Branch:</strong> {props.cardUser?.branch}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="body1" style={contentStyle}>
+              <strong>Batch:</strong> {props.cardUser?.batch}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="body1" style={contentStyle}>
+              <strong>Organization:</strong>{" "}
+              {props.cardUser?.work.organization}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="body1" style={contentStyle}>
+              <strong>Location:</strong> {city}, {state}, {country}
+            </Typography>
+          </Grid>
+        </Grid>
       </CardContent>
       <CardActions style={{ justifyContent: "center" }}>
-        <IconButton color="primary" aria-label="Chat" onClick={handleChat}>
+        <IconButton color="primary" aria-label="Chat">
           <ChatIcon />
         </IconButton>
-        <IconButton color="primary" aria-label="Profile">
+        {/* Trigger ProfileDisplay dialog */}
+        <IconButton
+          color="primary"
+          aria-label="Profile"
+          onClick={handleProfile}
+        >
           <AccountCircleIcon />
         </IconButton>
       </CardActions>
+      {/* Display the profile dialog */}
+      {selectedUser && (
+        <ProfileDisplay
+          open={openProfile}
+          onClose={() => setOpenProfile(false)}
+          userData={selectedUser}
+        />
+      )}
     </Card>
   );
 };
