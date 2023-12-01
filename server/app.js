91c673280830e2376f9595889f2de780134760f9
abhit-rana <abhit20421@iiitd.ac.in>
2023-12-02 01:39:31 +0530

    bugs(Chat Component): removed cors error

--- server/app.js
+++ server/app.js
@@ -6,12 +6,12 @@ import messageRouter from "./routes/messageRoutes.js";
 import eventRouter from "./routes/eventRouter.js";
 import cookieParser from "cookie-parser";
 import filterRouter from "./routes/filterRouter.js";
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from "@auth0/auth0-react";
 import notificationRouter from "./routes/notificationsRoutes.js";
 import cors from "cors";
-import { google } from 'googleapis'
-import dayjs from 'dayjs'
-import { v4 as uuid } from 'uuid'
+import { google } from "googleapis";
+import dayjs from "dayjs";
+import { v4 as uuid } from "uuid";
 
 const app = express();
 
@@ -27,42 +27,51 @@ config({
 // );
 
 const scopes = [
-  'https://www.googleapis.com/auth/calendar',
+  "https://www.googleapis.com/auth/calendar",
   // 'https://www.googleapis.com/auth/calendar.events'
 ];
 
 const allowedOrigins = [
-  'http://localhost:5000',
-  'http://localhost:4000',
-  'http://localhost:4000/schedule_event',
-  'http://localhost:4000/google/redirect',
-  'https://accounts.google.com/o/oauth2/v2/auth',
-  'https://accounts.google.com',
-  'http://www.google.com/support/accounts/bin/answer.py?hl=en&answer=151657',
-  'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar&response_type=code&client_id=205852059308-9052ffinaa09obcr0r23vibubi2963m5.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A4000%2Fgoogle%2Fredirect',
+  "http://localhost:5000",
+  "http://localhost:4000",
+  "http://localhost:4000/schedule_event",
+  "http://localhost:4000/google/redirect",
+  "https://accounts.google.com/o/oauth2/v2/auth",
+  "https://accounts.google.com",
+  "http://www.google.com/support/accounts/bin/answer.py?hl=en&answer=151657",
+  "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar&response_type=code&client_id=205852059308-9052ffinaa09obcr0r23vibubi2963m5.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A4000%2Fgoogle%2Fredirect",
 ];
 
-
-//adding middlewares
-app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", ["http://localhost:5000", "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar&response_type=code&client_id=205852059308-9052ffinaa09obcr0r23vibubi2963m5.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A4000%2Fgoogle%2Fredirect"]);
-  const origin = req.headers.origin;
-  // console.log("origin", origin);
-  if (allowedOrigins.includes(origin)) {
-    // Set the Access-Control-Allow-Origin header to the request's origin
-    // console.log("hello");
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  res.header("Access-Control-Allow-Origin", "http://localhost:5000");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Headers", "Content-Type,  Authorization");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Expose-Headers", "set-cookie");
   next();
 });
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", [
+//     "http://localhost:5000",
+//     "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar&response_type=code&client_id=205852059308-9052ffinaa09obcr0r23vibubi2963m5.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A4000%2Fgoogle%2Fredirect",
+//   ]);
+//   const origin = req.headers.origin;
+//   // console.log("origin", origin);
+//   if (allowedOrigins.includes(origin)) {
+//     // Set the Access-Control-Allow-Origin header to the request's origin
+//     // console.log("hello");
+//     res.header("Access-Control-Allow-Origin", origin);
+//   }
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Expose-Headers", "set-cookie");
+//   next();
+// });
 // app.use(cors());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/conversations", conversationRouter);
@@ -96,32 +105,33 @@ app.get("/google", (req, res) => {
   res.redirect(url);
 });
 
-app.get('/google/redirect', (req, res) => {
+app.get("/google/redirect", (req, res) => {
   const code = req.query.code;
-  oauth2Client.getToken(code)
-  .then(({ tokens }) => {
-    console.log("hello token", tokens);
-    oauth2Client.setCredentials(tokens);
-    res.send({
-      msg: 'Successfully logged in',
+  oauth2Client
+    .getToken(code)
+    .then(({ tokens }) => {
+      console.log("hello token", tokens);
+      oauth2Client.setCredentials(tokens);
+      res.send({
+        msg: "Successfully logged in",
+      });
+    })
+    .catch((error) => {
+      // Handle error appropriately
+      console.error("Error getting tokens:", error);
+      res.status(500).send({ error: "Failed to get tokens" });
     });
-  })
-  .catch(error => {
-    // Handle error appropriately
-    console.error('Error getting tokens:', error);
-    res.status(500).send({ error: 'Failed to get tokens' });
-  });
   res.redirect("/schedule_event");
 });
 
-app.use('/api/v1/saveEvent', eventRouter);
-app.use('/api/v1/fetchSlots', eventRouter);
+app.use("/api/v1/saveEvent", eventRouter);
+app.use("/api/v1/fetchSlots", eventRouter);
 
 // app.get('/schedule_event', async (req, res) => {
 //   try {
 //     const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
 //     console.log("schedule_event");
-  
+
 //     const event = await calendar.events.insert({
 //       calendarId: 'primary',
 //       resource: {
@@ -161,14 +171,13 @@ app.use('/api/v1/fetchSlots', eventRouter);
 // }
 
 // app.get('/schedule_event', async (req, res) => {
-  
+
 //   const token = req.headers.authorization; // Get the token from the request headers
 //   console.log("hello");
 //   if (!token) {
 //     return res.status(401).json({ message: 'Unauthorized: Missing token' });
 //   }
-app.post('/schedule_event', async (req, res) => {
-
+app.post("/schedule_event", async (req, res) => {
   console.log("hello schedule");
   // oauth2Client.setCredentials(
   //   {
