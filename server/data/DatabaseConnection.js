8bc1c7cdcd04e42c3f57c95b1c0c1347c104703f
HarshitJain-1908 <harshit20063@iiitd.ac.in>
2024-02-05 14:46:08 +0530

    deployment changes

--- server/data/DatabaseConnection.js
+++ server/data/DatabaseConnection.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  console.log("Connecting to database", process.env.MONGO_URI)
   await mongoose
     .connect(process.env.MONGO_URI, {
       dbName: "mentorship_portal",
